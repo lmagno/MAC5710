ad5169fea78c92a787181c66e2ce8ee0c0ef8837
Lucas Magno <lmagno@ime.usp.br>
2017-04-18 16:12:55 -0300

    Add coments and refactor a bit

--- EP2/bst.c
+++ EP2/bst.c
@@ -1,8 +1,15 @@
 #include "bst.h"
 
+/* Internal functions */
+Node** _search(Node **root, Key *key);
+void _traverse(Node *n, void (*callback)(Node *n));
+
+
 /******************************************************************************/
 /*                         Problem specific datatypes                         */
 /******************************************************************************/
+
+/* Structs for key and value of a node' */
 struct Key {
     int8_t cnt[26];
 };
@@ -11,12 +18,12 @@ struct Value {
     Queue *queue;
 };
 
+/* Allocate a key */
 Key* key_create(int8_t cnt[26]) {
     int i;
     Key *k;
 
     k = (Key*)malloc(sizeof(Key));
-    // k->cnt = (int*)malloc(26*sizeof(int));
 
     for(i = 0; i < 26; i++)
         k->cnt[i] = cnt[i];
@@ -24,6 +31,7 @@ Key* key_create(int8_t cnt[26]) {
     return k;
 }
 
+/* Allocate a value */
 Value* value_create() {
     Value *v;
 
@@ -33,6 +41,10 @@ Value* value_create() {
     return v;
 }
 
+/* Compare two keys, returning
+    -1 if k1 < k2
+     0 if k1 = k2
+     1 if k1 > k2 */
 int key_cmp(Key *k1, Key *k2) {
     int i;
 
@@ -46,6 +58,7 @@ int key_cmp(Key *k1, Key *k2) {
     return 0;
 }
 
+/* Struct for a Node */
 struct Node {
     Key *key;
     Value *value;
@@ -53,6 +66,7 @@ struct Node {
     Node *left, *right;
 };
 
+/* Allocate a node */
 Node* node_create(int8_t cnt[26]) {
     Node *n;
 
@@ -66,6 +80,7 @@ Node* node_create(int8_t cnt[26]) {
     return n;
 }
 
+/* Deallocate a node */
 void node_free(Node *n) {
     if(!n) return;
 
@@ -79,6 +94,7 @@ void node_free(Node *n) {
     free(n);
 }
 
+/* Callback function used during a traversal of the BST */
 void callback(Node *n) {
     int i;
     for(i = 0; i < 26; i++)
@@ -88,6 +104,7 @@ void callback(Node *n) {
     queue_print(n->value->queue);
 }
 
+/* Getter for queue (value) of a node */
 Queue* node_get_queue(Node *n) {
     return n->value->queue;
 }
@@ -95,10 +112,12 @@ Queue* node_get_queue(Node *n) {
 /*                                 Generic BST                                */
 /******************************************************************************/
 
+/* Struct for a Binary Search Tree */
 struct BST {
     Node *root;
 };
 
+/* Allocate a BST */
 BST* bst_create() {
     BST *b;
 
@@ -109,59 +128,65 @@ BST* bst_create() {
     return b;
 }
 
-Node* _insert(Node *root, Node *n) {
-    if(!root)
-        root = n;
-    else if(key_cmp(n->key, root->key) < 0)
-        root->left  = _insert(root->left, n);
-    else
-        root->right = _insert(root->right, n);
-
-    return root;
-}
-
-void bst_insert(BST *b, Node *n) {
-    b->root = _insert(b->root, n);
-}
-
-Node** _search(Node **root, Key *key) {
-    int cmp;
-    Node **m, *n = *root;
-
-    if(!n)
-        return NULL;
-
-    cmp = key_cmp(key, n->key);
-
-    if(cmp == 0)
-        return root;
-    else if(cmp < 0) {
-        m = _search(&(n->left), key);
-
-        if(!m)
-            return &(n->left);
-        else
-            return m;
-    } else
-        m = _search(&(n->right), key);
-
-        if(!m)
-            return &(n->right);
-        else
-            return m;
-}
-
 /* Searches the BST 'b' for the key 'key'.
-    If 'b' is empty, returns a pointer to the root.
-    If it's not empty, recursively searches for a node with
-    the same key, starting with the root. If it finds it,
-    a pointer to this node is returned, otherwise a pointer
-    to where it should be is returned.*/
+If 'b' is empty, returns a pointer to the root.
+If it's not empty, recursively searches for a node with
+the same key, starting with the root. If it finds it,
+a pointer to this node is returned, otherwise a pointer
+to where it should be is returned.*/
 Node** bst_search(BST *b, Key *key) {
     if(!(b->root))
         return &(b->root);
     else
         return _search(&(b->root), key);
+}
+
+Node** _search(Node **root, Key *key) {
+    int cmp;
+    Node *n = *root;
+
+    /* Compare the key with the current node's one */
+    cmp = key_cmp(key, n->key);
+
+    if(cmp == 0) {
+        /* If the keys are equal, return a pointer
+        to the current node */
+        return root;
+    } else if(cmp < 0) {
+        /* If the key is smaller than the current node's,
+        then look for it in the left subtree. If it is empty,
+        return a pointer to its root, because that's where a node
+        with this key should be inserted */
+        if(!n->left)
+            return &(n->left);
+        else
+            return _search(&(n->left), key);
+    } else {
+        /* If the key is bigger than the current node's,
+        then look for it in the right subtree. If it is empty,
+        return a pointer to its root, because that's where a node
+        with this key should be inserted */
+        if(!n->right)
+            return &(n->right);
+        else
+            return _search(&(n->right), key);
+    }
+}
+
+/* Insert a new node in the BST 'b' if there isn't a node
+with the same key already */
+void bst_insert(BST *b, Node *n) {
+    Node **m;
+
+    m = bst_search(b, n->key);
+    if(!(*m))
+        *m = n;
+}
+
+/* Recursively traverse the BST 'b' starting with the root,
+calling the function 'callback' each node.*/
+void bst_traverse(BST *b) {
+    _traverse(b->root, callback);
 }
 
 void _traverse(Node *n, void (*callback)(Node *n)) {
@@ -172,46 +197,8 @@ void _traverse(Node *n, void (*callback)(Node *n)) {
     _traverse(n->right, callback);
 }
 
-
-void bst_traverse(BST *b) {
-    _traverse(b->root, callback);
-}
-
+/* Deallocate the BST 'b' */
 void bst_free(BST *b) {
     node_free(b->root);
     free(b);
 }
-
-
-
-/******************************************************************************/
-/*                                   Testing                                  */
-/******************************************************************************/
-// void f(Node *n) {
-//     int i;
-//     for(i = 0; i < 26; i++)
-//         printf("%d ", n->key->cnt[i]);
-//
-//     printf("| %d", n->value->value);
-// }
-//
-// int main(int argc, char const *argv[]) {
-//     BST *b;
-//
-//     b = bst_create();
-//     bst_insert(b, node_create(5, 1));
-//     bst_insert(b, node_create(1, 2));
-//     bst_insert(b, node_create(4, 3));
-//     bst_insert(b, node_create(8, 4));
-//     bst_insert(b, node_create(3, 5));
-//     bst_insert(b, node_create(7, 6));
-//     bst_insert(b, node_create(9, 7));
-//     bst_insert(b, node_create(2, 8));
-//     bst_insert(b, node_create(6, 9));
-//
-//     bst_traverse(b, &f);
-//     printf("\n");
-//
-//     bst_free(b);
-//     return 0;
-// }
